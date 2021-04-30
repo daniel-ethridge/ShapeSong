256a0ec26f123d6023719f36999c7c8934519edb
Daniel Ethirdge <ethridge.daniel.m@gmail.com>
2021-04-30 14:55:34 -0400

    End of semester submission

---
+++ 7100Unity/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,275 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID A3 = 1886858601U;
+        static const AkUniqueID A4 = 1886858606U;
+        static const AkUniqueID C4 = 1853303296U;
+        static const AkUniqueID C5 = 1853303297U;
+        static const AkUniqueID CLAP = 3737052859U;
+        static const AkUniqueID CLAPSTOP = 518504891U;
+        static const AkUniqueID COWBELL = 772593125U;
+        static const AkUniqueID COWBELLSTOP = 4284949629U;
+        static const AkUniqueID D4 = 1836525717U;
+        static const AkUniqueID D5 = 1836525716U;
+        static const AkUniqueID F4 = 1802970447U;
+        static const AkUniqueID G4 = 1786192860U;
+        static const AkUniqueID HI_HAT = 1413521402U;
+        static const AkUniqueID HI_HATSTOP = 1232542626U;
+        static const AkUniqueID HIGH_TOM = 3594704544U;
+        static const AkUniqueID HIGH_TOMSTOP = 2107740072U;
+        static const AkUniqueID KICK = 2181839183U;
+        static const AkUniqueID KICKSTOP = 1952917439U;
+        static const AkUniqueID LOW_TOM = 2988693636U;
+        static const AkUniqueID LOW_TOMSTOP = 3440400492U;
+        static const AkUniqueID MID_TOM = 1675512312U;
+        static const AkUniqueID MID_TOMSTOP = 3948058160U;
+        static const AkUniqueID PLAYHIGHBKGND = 1997157647U;
+        static const AkUniqueID PLAYLOWBKGND = 127615467U;
+        static const AkUniqueID PLAYMIDBKGND = 3380058807U;
+        static const AkUniqueID SNARE = 3607016552U;
+        static const AkUniqueID SNARESTOP = 740187456U;
+        static const AkUniqueID STOPHIGHBKGND = 4024814773U;
+        static const AkUniqueID STOPLOWBKGND = 2892674729U;
+        static const AkUniqueID STOPMIDBKGND = 3507497869U;
+        static const AkUniqueID STOPNOTES = 3542992004U;
+        static const AkUniqueID STOPTIMER = 3754215428U;
+        static const AkUniqueID TIMER = 3920142940U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace BKGND_HIGH
+        {
+            static const AkUniqueID GROUP = 989654088U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BKGND_HIGH1 = 606598505U;
+                static const AkUniqueID BKGND_HIGH2 = 606598506U;
+                static const AkUniqueID BKGND_HIGH3 = 606598507U;
+                static const AkUniqueID BKGND_HIGH4 = 606598508U;
+                static const AkUniqueID BKGND_HIGH5 = 606598509U;
+            } // namespace SWITCH
+        } // namespace BKGND_HIGH
+
+        namespace BKGND_LOW
+        {
+            static const AkUniqueID GROUP = 1196574546U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BKGND_LOW1 = 2558936615U;
+                static const AkUniqueID BKGND_LOW2 = 2558936612U;
+                static const AkUniqueID BKGND_LOW3 = 2558936613U;
+                static const AkUniqueID BKGND_LOW4 = 2558936610U;
+                static const AkUniqueID BKGND_LOW5 = 2558936611U;
+            } // namespace SWITCH
+        } // namespace BKGND_LOW
+
+        namespace BKGND_MID
+        {
+            static const AkUniqueID GROUP = 1700050066U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BKGND_MID1 = 374882791U;
+                static const AkUniqueID BKGND_MID2 = 374882788U;
+                static const AkUniqueID BKGND_MID3 = 374882789U;
+                static const AkUniqueID BKGND_MID4 = 374882786U;
+                static const AkUniqueID BKGND_MID5 = 374882787U;
+            } // namespace SWITCH
+        } // namespace BKGND_MID
+
+        namespace CLAP
+        {
+            static const AkUniqueID GROUP = 3737052859U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CLAP1 = 1636120656U;
+                static const AkUniqueID CLAP2 = 1636120659U;
+                static const AkUniqueID CLAP3 = 1636120658U;
+                static const AkUniqueID CLAP4 = 1636120661U;
+                static const AkUniqueID CLAP5 = 1636120660U;
+            } // namespace SWITCH
+        } // namespace CLAP
+
+        namespace COWBELL
+        {
+            static const AkUniqueID GROUP = 772593125U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID COWBELL1 = 1664399182U;
+                static const AkUniqueID COWBELL2 = 1664399181U;
+                static const AkUniqueID COWBELL3 = 1664399180U;
+                static const AkUniqueID COWBELL4 = 1664399179U;
+                static const AkUniqueID COWBELL5 = 1664399178U;
+            } // namespace SWITCH
+        } // namespace COWBELL
+
+        namespace HI_HAT
+        {
+            static const AkUniqueID GROUP = 1413521402U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID HIHAT1 = 3897102192U;
+                static const AkUniqueID HIHAT2 = 3897102195U;
+                static const AkUniqueID HIHAT3 = 3897102194U;
+                static const AkUniqueID HIHAT4 = 3897102197U;
+                static const AkUniqueID HIHAT5 = 3897102196U;
+            } // namespace SWITCH
+        } // namespace HI_HAT
+
+        namespace HIGHTOM
+        {
+            static const AkUniqueID GROUP = 3434438675U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID HIGHTOM1 = 1418083800U;
+                static const AkUniqueID HIGHTOM2 = 1418083803U;
+                static const AkUniqueID HIGHTOM3 = 1418083802U;
+                static const AkUniqueID HIGHTOM4 = 1418083805U;
+                static const AkUniqueID HIGHTOM5 = 1418083804U;
+            } // namespace SWITCH
+        } // namespace HIGHTOM
+
+        namespace KICK
+        {
+            static const AkUniqueID GROUP = 2181839183U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID KICK1 = 138295148U;
+                static const AkUniqueID KICK2 = 138295151U;
+                static const AkUniqueID KICK3 = 138295150U;
+                static const AkUniqueID KICK4 = 138295145U;
+                static const AkUniqueID KICK5 = 138295144U;
+            } // namespace SWITCH
+        } // namespace KICK
+
+        namespace LOWTOM
+        {
+            static const AkUniqueID GROUP = 3877275895U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID LOWTOM1 = 3318062308U;
+                static const AkUniqueID LOWTOM2 = 3318062311U;
+                static const AkUniqueID LOWTOM3 = 3318062310U;
+                static const AkUniqueID LOWTOM4 = 3318062305U;
+                static const AkUniqueID LOWTOM5 = 3318062304U;
+            } // namespace SWITCH
+        } // namespace LOWTOM
+
+        namespace MIDTOM
+        {
+            static const AkUniqueID GROUP = 509081035U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID MIDTOM1 = 2407001760U;
+                static const AkUniqueID MIDTOM2 = 2407001763U;
+                static const AkUniqueID MIDTOM3 = 2407001762U;
+                static const AkUniqueID MIDTOM4 = 2407001765U;
+                static const AkUniqueID MIDTOM5 = 2407001764U;
+            } // namespace SWITCH
+        } // namespace MIDTOM
+
+        namespace SNARE
+        {
+            static const AkUniqueID GROUP = 3607016552U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID SNARE1 = 3673554825U;
+                static const AkUniqueID SNARE2 = 3673554826U;
+                static const AkUniqueID SNARE3 = 3673554827U;
+                static const AkUniqueID SNARE4 = 3673554828U;
+                static const AkUniqueID SNARE5 = 3673554829U;
+            } // namespace SWITCH
+        } // namespace SNARE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BKGNDSATURATION = 467147773U;
+        static const AkUniqueID CBPOSITION = 620741071U;
+        static const AkUniqueID CBSWITCH = 1742003886U;
+        static const AkUniqueID CBVOLUME = 2643497324U;
+        static const AkUniqueID CHARACTERARMS = 3261019387U;
+        static const AkUniqueID CHARACTERHEAD = 3364766416U;
+        static const AkUniqueID CHARACTERTORSO = 151387591U;
+        static const AkUniqueID CHARCTERLEGS = 3868248170U;
+        static const AkUniqueID CHARXPOS = 342993035U;
+        static const AkUniqueID CLPPOSITION = 3046941273U;
+        static const AkUniqueID CLPSWITCH = 2302862684U;
+        static const AkUniqueID CLPVOLUME = 1733953454U;
+        static const AkUniqueID HHPOSITION = 3791830556U;
+        static const AkUniqueID HHSWITCH = 3233319005U;
+        static const AkUniqueID HHVOLUME = 1515143787U;
+        static const AkUniqueID HIGH_BKGND_PARAM = 1538253904U;
+        static const AkUniqueID HIGHBRIGHT = 1965045917U;
+        static const AkUniqueID HIGHREVERB = 2840259689U;
+        static const AkUniqueID HTPOSITION = 2177393888U;
+        static const AkUniqueID HTSWITCH = 3834167089U;
+        static const AkUniqueID HTVOLUME = 2788579711U;
+        static const AkUniqueID KDPOSITION = 1003861849U;
+        static const AkUniqueID KDSWITCH = 3179197020U;
+        static const AkUniqueID KDVOLUME = 2610287790U;
+        static const AkUniqueID LOW_BKGND_PARAM = 762561156U;
+        static const AkUniqueID LOWBRIGHT = 3530131953U;
+        static const AkUniqueID LOWREVERB = 775711541U;
+        static const AkUniqueID LTPOSITION = 3117805916U;
+        static const AkUniqueID LTSWITCH = 2033904029U;
+        static const AkUniqueID LTVOLUME = 1016379563U;
+        static const AkUniqueID MID_BKGND_PARAM = 75764360U;
+        static const AkUniqueID MIDBRIGHT = 3930644517U;
+        static const AkUniqueID MIDREVERB = 2393634833U;
+        static const AkUniqueID MTPOSITION = 2871355627U;
+        static const AkUniqueID MTSWITCH = 1047568026U;
+        static const AkUniqueID MTVOLUME = 609655080U;
+        static const AkUniqueID PLAYBACK_SPEED = 78830142U;
+        static const AkUniqueID SNPOSITION = 3854265795U;
+        static const AkUniqueID SNRSWITCH = 878619056U;
+        static const AkUniqueID SNRVOLUME = 3625313298U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID BACKGROUNDS = 232237920U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MELODY = 1040548709U;
+        static const AkUniqueID PERCUSSION = 3850444438U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
